a369a4e2bd964329157f4b4613245abbb67b5dd5
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2019-01-09 10:09:54 -0400

    Se hizo la pantalla del juego,se agrego el snake y el punto y se actualizaron las instrucciones

--- SP.py
+++ SP.py
@@ -12,10 +12,37 @@ AB = Tkinter.Label(Instrucciones,text="Tiene 3 vidas no choques con las paredes"
 AB.pack()
 AC = Tkinter.Label(Instrucciones,text="Comete todos los puntos rojos")
 AC.pack()
-AD = Tkinter.Label(Instrucciones,text="Si quieres poner un nombre abajo")
-AD.pack()
-AE = Tkinter.Entry()
-AE.pack()
 Iniciar = Tkinter.Button(Instrucciones,text="Iniciar",command=quit)
 Iniciar.pack()
 Instrucciones.mainloop()
+
+pygame.init()
+pantalla = pygame.display.set_mode((1024,600))
+salir = False
+relog1 = pygame.time.Clock()
+rojo = (200,50,20)
+azul = (70,20,100)
+gris = (1,1,1)
+puntos = 0
+x = 400
+y = 100
+a = 100
+b = 100
+fuente1 = pygame.font.SysFont("Arial", 20, True, False)
+info = fuente1.render("Comete todos los puntos ", 0, (255,255,255))
+infb = fuente1.render("Tus puntos son:"+str(puntos), 0, (255,255,255))
+infa = fuente1.render("No choques", 0, (255,255,255))
+recta = pygame.Rect(x,y,60,15)
+rectb = pygame.Rect(a,b,15,15)
+while salir != True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			salir = True
+	pygame.display.update()
+	pantalla.fill(gris)
+	pantalla.blit(info,(5,5))
+	pantalla.blit(infb,(400,5))
+	pantalla.blit(infa,(700,5))
+	pygame.draw.rect(pantalla,azul,recta)	
+	pygame.draw.rect(pantalla,rojo,rectb)	
+pygame.quit()
