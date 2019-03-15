17c5aa170252d6dd0887a9f07ec1909f3ddfc805
Jesus Angulo Pulido <jesus.flores.angulo@gmail.com>
2019-03-15 17:17:18 -0400

    Se estan resolviendo problemas sobre las sintaxis y etc.

--- SP.py
+++ SP.py
@@ -25,7 +25,7 @@ rojo = (200,50,20)
 azul = (70,20,100)
 blanco = (200,200,200)
 puntos = 0
-puntosv = 0
+puntosv = 15
 y1 = 0
 x1 = 0
 listasp = []
@@ -33,8 +33,6 @@ arriba = False
 abajo = False
 derecha = False
 izquierda = False
-wr = 15
-hr = 15	
 fuente1 = pygame.font.SysFont("Arial", 20, True, False)
 infb = fuente1.render("Tus puntos son:"+str(puntos), 0, (132,12,11))
 for x in range(1):
@@ -43,8 +41,7 @@ for x in range(1):
 	x = random.randrange(300)
 	y = random.randrange(300)
 	listasp.append(pygame.Rect(x,y,w,h))
-recta = pygame.Rect(400,100,15,15)
-rects = pygame.Rect(recta.left,recta.top,15,15)
+recta = pygame.Rect(400,100,30,15)
 rectb = pygame.Rect(x,y,w,h)
 while salir != True:	
 	for event in pygame.event.get():
@@ -70,21 +67,16 @@ while salir != True:
 				derecha = True
 				izquierda = False
 				arriba = False
-				abajo = False
-		if recta.colliderect(rectb) and rects.colliderect(rectb):
-			rects.width += 15
-			if arriba:
-				print(recta.top)
-		
-			for x in range(1):
+				abajo = False       
+		if recta.colliderect(rectb):
+		    for x in range(1):
 				w = random.randrange(14,15)
 				h = random.randrange(14,15)
 				x = random.randrange(300)
-				y = random.randrange(300)	
-				listasp.append(pygame.Rect(x,y,w,h))	
-			rectb = pygame.Rect(x,y,w,h)
-			puntos += 5
-			infb = fuente1.render("Tus puntos son:"+str(puntos), 0, (132,12,11))	
+				y = random.randrange(300)
+		    rectb = pygame.Rect(x,y,w,h)
+		    puntos += 5
+		    infb = fuente1.render("Tus puntos son:"+str(puntos), 0, (132,12,11))	
 		if recta.top > 500:
 			recta.top = 100
 			recta.left = 400
@@ -129,23 +121,18 @@ while salir != True:
 			wr = 0
 			infb = fuente1.render("Tus puntos son:"+str(puntos), 0, (132,12,11))
 	if arriba == True:
-		recta.move_ip(0,-10)
-		rects = pygame.Rect(recta.left,recta.top,rects.width,rects.height)
+		recta.top -= 10
 	if abajo == True:
-		recta.move_ip(0,+10)
-		rects = pygame.Rect(recta.left,recta.top,rects.width,rects.height)
+		recta.top += 10
 	if derecha == True:
-		recta.move_ip(+10,0)
-		rects = pygame.Rect(recta.left,recta.top,rects.width,rects.height)
+		recta.left += 10
 	if izquierda == True:
-		recta.move_ip(-10,0)	
-		rects = pygame.Rect(recta.left,recta.top,rects.width,rects.height)
+		recta.left -= 10
 
 	relog1.tick(15)
 	pygame.display.update()
 	pantalla.fill(blanco)
 	pantalla.blit(infb,(5,5))
-	pygame.draw.rect(pantalla,azul,recta)	
-	pygame.draw.rect(pantalla,azul,rects)	
+	pygame.draw.rect(pantalla,azul,recta)
 	pygame.draw.rect(pantalla,rojo,rectb)
 pygame.quit()
